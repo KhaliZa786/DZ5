d3673469aeaa990dd6cc0588eb5b6d85ac19461f
KhaliZa786 <121446863+KhaliZa786@users.noreply.github.com>
2023-05-02 16:02:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+a = int(input("Введите день "))
+if a == 1:
+    print("Понедельник")
+if a == 2:
+    print("Вторник")
+if a == 3:
+    print("Среда")
+elif a == 4:
+  print("Четверг")
+elif a == 5:
+    print("Пятница")
+elif a == 6:
+    print("Суббота")
+elif a == 7:
+    print("Воскресенье")
+
+a = int(input("Введите месяц"))
+if a == 1:
+    print("January")
+if a ==2:
+    print("Febrary")
+if a == 3:
+    print("Martch")
+if a == 4:
+    print("April")
+if a == 5:
+    print("May")
+if a == 6:
+    print("June")
+if a == 7:
+    print("July")
+if a == 8:
+    print("August")
+if a == 9:
+    print("September")
+if a == 10:
+    print("October")
+if a == 11:
+    print("November")
+if a == 12:
+    print("Desember")
+
+n = int(input("Введите число"))
+if n < 0:
+    print("Number is positive")
+elif n < 0:
+    print("Number is negative")
+elif n == 0:
+    print("Number is equal to zero")
